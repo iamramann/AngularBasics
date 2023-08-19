bd87404b0bcacb92ba15e19fe8154a9572f0aafe
Raman Sharma <kukreti.rs@gmail.com>
2023-08-19 14:34:17 +0530

    One way data binding and two way data binding.

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: `
-      <div class="alert alert-success text-center">
-        <p>This website uses cookies to provide better user experience.</p>
+      <div class="alert alert-success text-center" [hidden]="displayNotification">
+         This website uses cookies to provide better user experience. 
+        <div class="close" (click)="hideNotification()"><button class="btn">X</button></div>
       </div>  
   `,
-  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; text-align:center; background-color: #FAD7A0}", "p{font-size: 14px;}"],
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; text-align:center; background-color: #FAD7A0}", ".close { float: right;  margin-top: -15px;}", "p{font-size: 14px;}"],
 })
 export class NotificationComponent {
-
+  displayNotification: boolean = false;
+  hideNotification() {
+    this.displayNotification = true;
+  }
 }
