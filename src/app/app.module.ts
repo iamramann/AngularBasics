396ab8e1a511591f7187c9e4c6e647c399e7cec1
Raman Sharma <kukreti.rs@gmail.com>
2023-08-19 12:40:17 +0530

    Created custom component both manually and using angular cli.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
