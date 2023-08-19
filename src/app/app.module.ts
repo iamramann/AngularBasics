7e1c4b80f0945b3b155812971b24c70c13099254
Raman Sharma <kukreti.rs@gmail.com>
2023-08-19 12:51:43 +0530

    Used template property of a component instead of templateUrl.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
